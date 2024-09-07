b8f9a8c7477ddb92b21da47bfb86f54721919a65
pusp joshi <erpushparaj23@gmail.com>
2024-09-07 18:25:37 +0545

    end page

---
+++ end.js
@@ -0,0 +1,10 @@
+const username  = document.getElementById('username');
+const saveScoreBtn = document.getElementById('saveScoreBtn');
+
+username.addEventListener('keyup', () => {
+    console.log(username.value);
+});
+saveHighScore = (e) => {
+    console.log('clicked the save button');
+    e.preventDefault();
+}
