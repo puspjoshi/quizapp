bf7c24509fc834cac9096527d86589342d49a7ab
pusp joshi <erpushparaj23@gmail.com>
2024-09-07 18:37:24 +0545

    end page

--- game.js
+++ game.js
@@ -53,6 +53,7 @@ startGame = () => {
 getNewQuestion = () => {
     if(availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS){
         //go to the end page
+        localStorage.setItem("mostRecentScore", score);
         return window.location.assign('end.html');
     }
     questionCounter++;
