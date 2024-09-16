766febea360fc89417982a8e5a05da9d4252a9cb
pusp joshi <erpushparaj23@gmail.com>
2024-09-16 22:20:44 +0545

    questions create custom api

--- game.js
+++ game.js
@@ -11,32 +11,20 @@ let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
 
-let questions = [
-    {
-        question: "Inside which HTML element do we put the Javascript ?",
-        choice1: "<script>",
-        choice2: "<javascript>",
-        choice3: "<js>",
-        choice4: "<scripting>",
-        answer: 1
-    },
-    {
-        question: "What is the correct syntax for referring to an external script called xxx.js?",
-        choice1: "<script href ='xxx.js'>",
-        choice2: "<script name='xxx.js'>",
-        choice3: "<script src='xxx.js'>",
-        choice4: "<script file ='xxx.js'>",
-        answer: 3
-    },
-    {
-        question: "How do you write 'Hello world' in an alert box ?",
-        choice1: "msgBox('Hello World');",
-        choice2: "alertBox('Hello World');",
-        choice3: "msg('Hello World');",
-        choice4: "alert('Hello world');",
-        answer: 4
-    }
-];
+let questions = [];
+
+fetch("questions.json").then(res => {
+    console.log(res);
+    return res.json();
+})
+.then(loadedQuestions => {
+    console.log(loadedQuestions);
+    questions = loadedQuestions;
+    startGame();
+})
+.catch( err => {
+    console.log(err);
+});
 
 //constants
 
@@ -103,4 +91,3 @@ incrementScore = num => {
     score += num;
     scoreText.innerText = score;
 }
-startGame();
